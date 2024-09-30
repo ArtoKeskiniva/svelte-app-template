18f5749d27a7ec0d9a26565fc182eabe95cf8a0f
Mellis <mellis@iki.fi>
2024-09-30 21:10:35 +0300

    Move files around, add .editorconfig

--- template/vite.config.js
+++ template/vite.config.js
@@ -4,9 +4,19 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	root: "src/",
+	publicDir: "../public",
+	base: "/",
+	build: {
+		outDir: "../dist",
+		emptyOutDir: true
+	},
 	server: {
 		host: 'localhost',
 		port: 54320
 	},
-	base: "/"
+	preview: {
+		host: 'localhost',
+		port: 54320
+	}
 })
